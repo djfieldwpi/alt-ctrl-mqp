7744c196ef2930eb507f7119569ac1e19992591d
Dennis Field <djfield@wpi.edu>
2026-01-20 16:27:57 -0500

    vertice file writing

--- External Software/test.py
+++ External Software/test.py
@@ -5,7 +5,11 @@ while True:
         if (f.readline().strip() == "GO"):
             f.close()
             with open("C:\\Users\\field\\Desktop\\College Documents\\MQP\\alt-ctrl-mqp\\signal.txt", 'w') as f:
-                f.write("DONE")
+                vertices = [(0, 0), (200, 0), (200, 200), (0, 200)]
+                vertices_str = '\n'.join([f"{x} {y}" for x, y in vertices])
+                
+                f.write("DONE\n" + vertices_str)
+                print("DONE\n" + vertices_str)
                 f.close()
 
     time.sleep(1)
