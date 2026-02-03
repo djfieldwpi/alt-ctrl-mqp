4190b35190211e30bcb946eaee87342062bb9859
Dennis Field <djfield@wpi.edu>
2026-02-03 13:47:29 -0500

    Camera script
    imported camera script logic
    added new global variable for tracking

---
+++ unstrung/Scripts/camera_2d.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+var character_start_x = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.make_current()
+	# character_start_x = "CharacterBody2D".global_position.x
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if GlobalVariables.is_camera_follow:
+		pass
+		# Mid-screen tracking (right)
+		# if "CharacterBody2D".global_position.x > self.global_position.x:
+		#	self.global_position.x = "CharacterBody2D".global_position.x
+		# Starting position tracking (left)
+		# elif self.global_position.x - "CharacterBody2D".global_position.x > 960 - character_start_x::
+		#	self.global_position.x = "CharacterBody2D".global_position.x + (960 - character_start_x)
--- unstrung/Scripts/global_variables.gd
+++ unstrung/Scripts/global_variables.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var is_actors_locked = false
+var is_camera_follow = false
