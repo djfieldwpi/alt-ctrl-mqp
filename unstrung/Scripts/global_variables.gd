ce5d54bee28eddbfd2011606b395dd1464322e32
Dennis Field <djfield@wpi.edu>
2026-02-11 15:28:02 -0500

    Z index fix

--- unstrung/Scripts/global_variables.gd
+++ unstrung/Scripts/global_variables.gd
@@ -4,4 +4,4 @@ var is_actors_locked: 	bool = false
 var is_system_lock:		bool = false
 var is_camera_follow: 	bool = false
 
-var debug_drawn_shadows: bool = false
+var debug_drawn_shadows: bool = true
