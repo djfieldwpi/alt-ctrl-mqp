e7ae523f50a50cd31d4e841b2d459a32d4268b26
Dennis Field <djfield@wpi.edu>
2026-02-12 12:20:48 -0500

    UI reintegration

--- unstrung/Scripts/ui.gd
+++ unstrung/Scripts/ui.gd
@@ -1,19 +1,27 @@
 extends Control
 
-@onready var main_menu: VBoxContainer = $MarginContainer/MainMenu
+@onready var main_menu: VBoxContainer = $MarginContainer/MarginContainerMainMenu/MainMenu
 @onready var options: Panel = $MarginContainer/Options
-
+@onready var credits: Panel = $MarginContainer/Credits
+@onready var controls: Panel = $MarginContainer/Controls 
 
 func _ready() -> void:
 	main_menu.visible = true
 	options.visible = false
-	
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/level_one.tscn")
+	credits.visible = false
+	controls.visible = false
 	
 func _on_settings_button_pressed() -> void:
 	main_menu.visible = false
 	options.visible = true
+	
+func _on_credits_button_pressed() -> void:
+	main_menu.visible = false
+	credits.visible = true
+	
+func _on_controls_button_pressed() -> void:
+	main_menu.visible = false
+	controls.visible = true
 	
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -24,6 +32,17 @@ func _on_check_button_toggled(toggled_on: bool) -> void:
 func _on_back_button_pressed() -> void:
 	main_menu.visible = true
 	options.visible = false
+	
+func _on_back_button_2_pressed() -> void:
+	main_menu.visible = true
+	credits.visible = false
+	
+func _on_back_button_3_pressed() -> void:
+	main_menu.visible = true
+	controls.visible = false
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/level_one.tscn")
