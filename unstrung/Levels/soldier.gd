c0f80e77a3c754ff390285a493fe6b0336885ef0
Dennis Field <djfield@wpi.edu>
2026-02-17 14:51:19 -0500

    Soldier and Level one edits

--- unstrung/Levels/soldier.gd
+++ unstrung/Levels/soldier.gd
@@ -4,12 +4,33 @@ const SPEED = 10000.0
 var right = true
 var dir
 
+@onready var ray: RayCast2D = $RayCast2D
+@onready var player: CharacterBody2D = %CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	if GlobalVariables.is_near_soldier:
+		ray.target_position = ray.to_local(player.global_position)
+		
+		if ray.is_colliding():
+			var collider = ray.get_collider()
+			
+			if collider == player:
+				print("I see the player!")
+				GlobalVariables.is_actors_locked = true
+				GlobalVariables.is_near_soldier = false
+				player.global_position = %Triggers.checkpoints[2]
+				var timer: SceneTreeTimer = get_tree().create_timer(2)
+				await timer.timeout
+				GlobalVariables.is_actors_locked = false
+			else:
+				print("No player in sight.")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if not GlobalVariables.is_actors_locked:
-		if right and global_position.x > 6830:
+		if right and global_position.x > 6515:
 			right = false
-		elif not right and global_position.x < 5400:
+		elif not right and global_position.x < 5570:
 			right = true
 		
 		if right:
