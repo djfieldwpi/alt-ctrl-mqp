9a8c1b742e6cf735d317dbf03c195463475ad8a4
Dennis Field <djfield@wpi.edu>
2026-03-04 12:00:48 -0500

    Soldier ray and visibility/transparency fixes

--- unstrung/Levels/soldier.gd
+++ unstrung/Levels/soldier.gd
@@ -5,11 +5,14 @@ var right = true
 var dir
 
 @onready var ray: RayCast2D = $RayCast2D
+@onready var line: Line2D = $Line2D
 @onready var player: CharacterBody2D = %CharacterBody2D
 
 func _physics_process(_delta: float) -> void:
 	if GlobalVariables.is_near_soldier:
 		ray.target_position = ray.to_local(player.global_position)
+		line.points[1] = to_local(player.global_position)
+		line.visible = true
 		
 		if ray.is_colliding():
 			var collider = ray.get_collider()
@@ -17,12 +20,15 @@ func _physics_process(_delta: float) -> void:
 			if collider == player:
 				print("I see the player!")
 				GlobalVariables.is_actors_locked = true
+				var timer: SceneTreeTimer = get_tree().create_timer(2)
+				await timer.timeout
 				GlobalVariables.is_near_soldier = false
 				player.global_position = %Triggers.checkpoints[2]
-				var timer: SceneTreeTimer = get_tree().create_timer(2)
+				timer = get_tree().create_timer(2)
 				await timer.timeout
 				GlobalVariables.is_actors_locked = false
 			else:
+				line.visible = false
 				print("No player in sight.")
 
 # Could remove movement? 
